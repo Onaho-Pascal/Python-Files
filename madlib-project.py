4bb859fed2b9bd06e6b96c2a727a98da2eb6c797
Pascal <Pascalonaho@gmail.com>
2024-05-09 09:53:54 +0100

    Started the Mad libs generator python project

--- madlib-project.py
+++ madlib-project.py
@@ -1,10 +1,5 @@
+with open("story.txt", "r") as f:
+    story = f.read()
 
+print(story)
 
-for i in range(2, 20, 2):
-    print(i, end=", ")
-
-
-test = [20, 40, 50, 60, 100, 120]
-
-for i in test:
-    print(i * 2, end=", ")
