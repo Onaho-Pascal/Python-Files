e8972e11d2db5b1a57447e74731255ef9907d4f5
Pascal <Pascalonaho@gmail.com>
2024-05-29 00:01:17 +0100

    Changes made in randomization file

--- Randomization.py
+++ Randomization.py
@@ -1,7 +1,23 @@
-import random
+# import random
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+# # random_integer = random.randint(1, 10)
+# # print(random_integer)
 
-random_float = random.random() * 5
-print(random_float)
+# # random_float = random.random() * 5
+# # print(random_float)
+
+
+# heads = 0
+# tails = 1
+# coin_flip = random.randint(0, 1)
+# if coin_flip == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+
+# Lists
+
+states_in_nigeria = ["Abia", "Adamawa", "Akwa-ibom", "Anambra", "Bauchi", "Bayelsa", "Benue", "Borno", "Cross-river", "Delta", "Ebonyi", "Edo", "Ekiti", "Enugu"]
+
+print(states_in_nigeria[2])
