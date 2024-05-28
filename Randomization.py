c8c9aa4eb79424d467cb55fefb8363e4b812b61d
Pascal <Pascalonaho@gmail.com>
2024-05-29 00:08:41 +0100

    updated randomization file

--- Randomization.py
+++ Randomization.py
@@ -1,19 +1,19 @@
-# import random
+import random
 
-# # random_integer = random.randint(1, 10)
-# # print(random_integer)
+random_integer = random.randint(1, 10)
+print(random_integer)
 
-# # random_float = random.random() * 5
-# # print(random_float)
+random_float = random.random() * 5
+print(random_float)
 
 
-# heads = 0
-# tails = 1
-# coin_flip = random.randint(0, 1)
-# if coin_flip == 0:
-#     print("Heads")
-# else:
-#     print("Tails")
+heads = 0
+tails = 1
+coin_flip = random.randint(0, 1)
+if coin_flip == 0:
+    print("Heads")
+else:
+     print("Tails")
 
 
 # Lists
