99e6eb3d4aa98e6357720e06ada61c8a8c6bbf27
Pascal <Pascalonaho@gmail.com>
2024-06-04 22:53:55 +0100

    Updated

--- Bioinformatics-file.py
+++ Bioinformatics-file.py
@@ -1,56 +1,59 @@
 Nucleotides = ["A", "C", "G", "T"]
 
-# checking the sequence to ensure it is a DNA string
 
+# checking the sequence to ensure it is a DNA string
+dna_seq = "c"
 def validateSeq(dna_seq):
     tnpseq = dna_seq.upper()
     for nuc in tnpseq:
         if nuc not in Nucleotides:
             return False
     return tnpseq
+    print(dna_seq)
+    
 
-#now, to generate a random sequence
+# #now, to generate a random sequence
 
-import random
+# import random
 
-rndDNAstring = "".join([random.choice(Nucleotides)
-                        for nuc in range(20)])
+# rndDNAstring = "".join([random.choice(Nucleotides)
+#                         for nuc in range(20)])
 
-print(validateSeq(rndDNAstring))
-
-
-#to test out this function
-
-#the ".upper()" function changes any lowercased letter in the string to uppercase and prints it out
-#as long as it is in the original dictonary of Nucleotides as given above
-
-random_DNAstrg1 = "ACCGTAGTGACGGTGT"
-
-random_DNAstrg2 = "CCTGCTAcctactg"
-
-print(validateSeq(random_DNAstrg1))
-print(validateSeq(random_DNAstrg2))
-
-#the "upper.()" function allows python to convert the given string into uppercase letters,
-#then checks to see if they are among the given sting or dictonary in the firstline
-random_DNAstrg1 = "CGGTAAATGCAACG"
-random_DNAstrg2 = "CcgtGacGTGCacatTacG"
-random_DNAstrg3 = "SHGBTACGTCAAGCTACTCTGA"
+# print(validateSeq(rndDNAstring))
 
 
+# #to test out this function
 
-#print(validateSeq(random_DNAstrg1))
-#print(validateSeq(random_DNAstrg2))
-#print(validateSeq(random_DNAstrg3))
+# #the ".upper()" function changes any lowercased letter in the string to uppercase and prints it out
+# #as long as it is in the original dictonary of Nucleotides as given above
+
+# random_DNAstrg1 = "ACCGTAGTGACGGTGT"
+
+# random_DNAstrg2 = "CCTGCTAcctactg"
+
+# print(validateSeq(random_DNAstrg1))
+# print(validateSeq(random_DNAstrg2))
+
+# #the "upper.()" function allows python to convert the given string into uppercase letters,
+# #then checks to see if they are among the given sting or dictonary in the firstline
+# random_DNAstrg1 = "CGGTAAATGCAACG"
+# random_DNAstrg2 = "CcgtGacGTGCacatTacG"
+# random_DNAstrg3 = "SHGBTACGTCAAGCTACTCTGA"
 
 
-# now to create a random DNA sequence for testing and counting
 
-def countnucfrequency(seq):
-    tmpfreqdict = {"A": 0, "C": 0, "G": 0, "T":0}
-    for nuc in seq:
-        tmpfreqdict[nuc] += 1
-    return tmpfreqdict
+# #print(validateSeq(random_DNAstrg1))
+# #print(validateSeq(random_DNAstrg2))
+# #print(validateSeq(random_DNAstrg3))
 
-print(countnucfrequency(rndDNAstring))
+
+# # now to create a random DNA sequence for testing and counting
+
+# def countnucfrequency(seq):
+#     tmpfreqdict = {"A": 0, "C": 0, "G": 0, "T":0}
+#     for nuc in seq:
+#         tmpfreqdict[nuc] += 1
+#     return tmpfreqdict
+
+# print(countnucfrequency(rndDNAstring))
 
---
+++ loops.py
@@ -0,0 +1,3 @@
+Games = ["Mk1", "SF6", "Tekken"]
+for Game in Games:
+    print(Game)
