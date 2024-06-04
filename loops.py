066e980fdbb33331097b05e8663efb96829462c8
Pascal <Pascalonaho@gmail.com>
2024-06-04 23:40:20 +0100

    updated

--- loops.py
+++ loops.py
@@ -1,3 +1,11 @@
 Games = ["Mk1", "SF6", "Tekken"]
 for Game in Games:
     print(Game)
+total_height = 0
+students_height = [30, 27, 28, 22, 24, 29, 23]
+for height in students_height:
+    total_height += height
+print(total_height)
+
+for number in range(2, 22, 2):
+    print(number)
