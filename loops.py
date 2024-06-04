02ac58a8e92a734de9cc6e5e3de4eb32bfea895b
Pascal <Pascalonaho@gmail.com>
2024-06-05 00:27:13 +0100

    updated

--- loops.py
+++ loops.py
@@ -1,11 +1,31 @@
-Games = ["Mk1", "SF6", "Tekken"]
-for Game in Games:
-    print(Game)
-total_height = 0
-students_height = [30, 27, 28, 22, 24, 29, 23]
-for height in students_height:
-    total_height += height
-print(total_height)
+# Games = ["Mk1", "SF6", "Tekken"]
+# for Game in Games:
+#     print(Game)
+# total_height = 0
+# students_height = [30, 27, 28, 22, 24, 29, 23]
+# for height in students_height:
+#     total_height += height
+# print(total_height)
 
-for number in range(2, 22, 2):
-    print(number)
+# # Adding numbers from 1 to 100
+# total = 0
+# for number in range(1, 101):
+#     total += number
+# print(total)
+
+# # Adding even numbers from 1 to 100
+
+# even_numbers = 0
+# for even in range(2, 102, 2):
+#     even_numbers += even
+# print(even_numbers)
+
+for num in range(1, 101):
+    print(num)
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    
