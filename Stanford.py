e315e6350c7a3cfd75c27d7351caa65c74d160a3
Pascal <Pascalonaho@gmail.com>
2024-11-20 09:34:17 +0100

    updated

---
+++ Stanford.py
@@ -0,0 +1,9 @@
+game = "Brawlhalla"
+platform = "Playstation 4 or 5"
+
+print("The name of the video game is {}, and you can play it on {}." .format(game, platform))
+
+numbers = [1, 2, "three", 4, 5]
+numbers[2] = 3 #replacing the value "three" with "3" in a list
+numbers.append(6) #to add the number 6 to the list, I used the "append" function
+print(numbers)
---
+++ paulfile.py
@@ -0,0 +1,7 @@
+name = input("what is your name?")
+if name == "betronel_or_baul":
+    print("Welcome betronel_or_baul")
+else:
+    print("Not authorized")
+
+    
