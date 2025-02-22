932a3e5566d635e0df5bad147cd1d64519f8313e
Pascal <Pascalonaho@gmail.com>
2025-02-22 15:22:56 +0100

    updated

--- loop.py
+++ loop.py
@@ -1,29 +1,29 @@
-#friends = ["Dami", "AK", "Orobosa", "Tomisin"]
-#for friend in friends:
-#    print(friend + " " + "is a good friend")
+friends = ["Dami", "AK", "Orobosa", "Tomisin"]
+for friend in friends:
+    print(friend + " " + "is a good friend")
 
-#ca = 23
-#scores = [61, 53, 67, 64, 59]
-#for score in scores:
- #   score += ca
-  #  print(score)
+ca = 23
+scores = [61, 53, 67, 64, 59]
+for score in scores:
+    score += ca
+    print(score)
 
-#val = 3
+val = 3
 
-#print(val)
+print(val)
 
-#max_score = 0
-#scores = [61, 53, 67, 64, 59]
+max_score = 0
+scores = [61, 53, 67, 64, 59]
 
-#for score in scores:
- ##      max_score = score
+for score in scores:
+       max_score += score
 
-#print(max_score)
+print(max_score)
 
-#constant = 0
-#for number in range(1, 101):
- #   constant += number
-#print(constant)
+constant = 0
+for number in range(1, 101):
+    constant += number
+print(constant)
 
 for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
